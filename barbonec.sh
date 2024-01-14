cb4fca80876a90e3f2bd52420128380040960be6
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-14 18:48:59 +0000

    Add files via upload

--- barbonec.sh
+++ barbonec.sh
@@ -1,6 +1,6 @@
 rm kernel.bin 2>/tmp/null
-as -o /tmp/boot.o boot.s
+as -o /tmp/boot.o ./file/boot.s
 gcc -c -nostdlib $1 -o /tmp/kernel.o
-ld -T link.ld -nostdlib /tmp/boot.o /tmp/kernel.o -o /tmp/kernel.bin
+ld -T ./file/link.ld -nostdlib /tmp/boot.o /tmp/kernel.o -o /tmp/kernel.bin
 grub-file --is-x86-multiboot /tmp/kernel.bin
 mv /tmp/kernel.bin ./
