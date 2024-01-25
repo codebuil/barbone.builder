d8e0d1c8a582fc6c3976eadfcb6a3869b24a9f51
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-25 11:48:27 +0000

    Add files via upload

--- file/kernel.c
+++ file/kernel.c
@@ -1,63 +1,5 @@
-//gcc -c -o emulator.o emulator.c
-//gcc -T emulink.ld -o os.exe -nostdlib emulator.o -lgcc
+#include <base.h>
 
-
-#include <stdio.h>
-#include <string.h>
-int xxx=0;
-int yyy=0;
-int zzz=0;
-char *scr2=(char *)0x000b8000L;//[80*26*2];
-
-void clear(){
-	int n=0;
-	char *src = scr2;//(char *)0x000b8000L;
-	for(n=0;n<80*24*2;n=n+2){
-		src[n]=32;
-		src[n+1]=0x67;
-	}
-}
-void scrollb8000()
-{
-	int n=0;
-	int nn=0;
-	
-	char *src = scr2;//(char *)0x000b8000L;
-	 
-	for(n=0;n<80*24*2;n++)src[n]=src[n+160];
-}
-void copyb8000(int address,char *s)
-{
-	int n=0;
-	int nn=0;
-	
-	char *src = scr2;//(char *)0x000b8000L;
-	 
-	while(s[n]!=0){
-		src[address+nn]=s[n];
-		nn++;
-		nn++;
-		n++;
-	}
-}
-void locate(int x,int y){
-
-	xxx=x;
-	yyy=y;
-	if(x>79)x=79;
-	if(y>24)y=24;
-	zzz=y*80*2+x*2;
-}
-void print(char* s){
-	copyb8000(zzz,s);
-	xxx=0;
-	yyy++;
-	if (yyy>24){
-		yyy=24;
-		scrollb8000();
-	}
-	locate(xxx,yyy);
-}
 void kernel_main()
 {
 	char *src =scr2; //(char *)0x000b8000L;
@@ -86,4 +28,5 @@ void kernel_main()
 	
 	
 }
+
 
